be3ab108b43eed27e749aa16e3483301375555c7
Umangsingh09 <rajumang74@gmail.com>
2026-03-15 00:03:29 +0530

    added gunicorn and whitenoise for deployment

--- triage/urls.py
+++ triage/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('analyze/', views.AnalyzeTriageView.as_view(), name='analyze'),
-    path('health-agent/', views.health_agent, name='health_agent'),
+    path('analyze/', views.AnalyzeTriageView.as_view(), name='analyze_triage'),
 ]
